7eb457e74d12c12b333122cf2bf3db1c39fe5a8d
AlexFursenkoOd <andersen.od.ua@gmail.com>
2018-11-07 09:02:19 +0200

    Add project files.

---
+++ TariffComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TariffComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
