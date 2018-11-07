6e05f231ede413560a6a59cc8d41b7b21d89e9a0
YaroslavGozak <gozakyaroslav@gmail.com>
2018-11-07 09:21:02 +0200

    - Addded IElectricityTariff interface - Added two tariff implementations - Added TariffComparisonResult model to hold tariff info for particular consumption - Added unit tests

---
+++ TariffComparison/IElectricityTariff.cs
@@ -0,0 +1,8 @@
+﻿namespace TariffComparison
+{
+    public interface IElectricityTariff
+    {
+        string Name { get; }
+        double GetAnnualCosts(double electricity);
+    }
+}
--- TariffComparison/Program.cs
+++ TariffComparison/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TariffComparison
 {
@@ -6,7 +8,22 @@ namespace TariffComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var comaprisonResults = CompareConsumptionCosts(new List<IElectricityTariff> { new BasicTariff(), new PackagedTariff() }, 4500);
+            Console.WriteLine("{0,25}: {1,13}", "Tariff name", "Annual costs");
+            foreach (var comparisonRow in comaprisonResults)
+            {
+                Console.WriteLine("{0,25}: {1,13:N2}", comparisonRow.Name, comparisonRow.AnnualCosts);
+            }
+            Console.ReadLine();
+        }
+
+        static IEnumerable<TariffComparisonResult> CompareConsumptionCosts(IEnumerable<IElectricityTariff> tariffs, double consumption)
+        {
+            return tariffs.Select(t => new TariffComparisonResult
+            {
+                Name = t.Name,
+                AnnualCosts = t.GetAnnualCosts(consumption)
+            }).OrderBy(tc => tc.AnnualCosts);
         }
     }
 }
